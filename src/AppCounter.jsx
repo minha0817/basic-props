a294bfad0c827728c1d3cc077744db550b810449
minha0817 <minhakk24@gmail.com>
2022-10-30 22:41:31 -0700

    add total count

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 import Counter from './components/Counter'
 
 export default function AppCounter() {
+
+    const handleClick = () => setCount(prev => prev + 1);
+
+    const [count, setCount] = useState(0);
+
     return (
-        <div>
-            <Counter />
-            <Counter />
+        <div className='container'>
+            <div className='banner'>
+                Total count: {count} {count > 10 ? '❤️‍🔥' : '❄️'}
+            </div>
+
+            <div className='counters'>
+                <Counter total={count} onClick={handleClick} />
+                <Counter total={count} onClick={handleClick}/>
+            </div>
 
         </div>
     );
